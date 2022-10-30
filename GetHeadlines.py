d483a889889b1f89a5876de587ea7afc7de69e29
anmaxwell <aniamaxwell@gmail.com>
2022-10-30 15:53:45 +0000

    converting headlines to vectors

--- GetHeadlines.py
+++ GetHeadlines.py
@@ -1,4 +1,9 @@
 # Imports
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+
+import json
 import os
 from datetime import datetime
 from newsapi import NewsApiClient
@@ -11,27 +16,33 @@ top_uk_headlines = newsapi.get_top_headlines(category=('business' or 'technology
                                           language='en',
                                           country='gb')
 
-# Create a list of the latest headlines to convert to vectors
+#Loading pretrained model
+module_url = 'https://tfhub.dev/google/universal-sentence-encoder/4'
+model = hub.load(module_url)
+
+#function to create embedding vectors
+def embed(input):
+    return model(input)
+
+# Create a list of the latest headlines converted to vectors
 headlines = []
 for item in top_uk_headlines['articles']:
-    headlines.append(item['title'])
+    headline_txt = item['title']
 
-# Convert headlines to vectors
+    #create vectors from questions
+    vector_USE = embed(headline_txt)
+    print('vector', vector_USE)
+
+    headlines.append(vector_USE)
 
 # Check if the directory is there to write the output file else create it
-path = '/ukheadlines'
-curr_timestamp = int(datetime.timestamp(datetime.now()))
+path = 'ukheadlines'
+filename = 'ukheadlines_'+str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+dirpath = os.path.join(path, filename + '.json')
 
-def createheadlinefile():
-    with open('ukheadlines/ukheadlines_{curr_timestamp}.json', 'w') as outfile:
-        json.dump(headlines, outfile)
-
-if os.path.exists(path):
-    with open('ukheadlines/ukheadlines_{curr_timestamp}.json', 'w') as outfile:
-        json.dump(headlines, outfile)
-
-else:
+if not os.path.exists(path):
     os.makedirs(path) 
-    with open('ukheadlines/ukheadlines_{curr_timestamp}.json', 'w') as outfile:
-        json.dump(headlines, outfile)
-    print('oh dear')
+  
+print('created: ',filename)
+with open(dirpath, 'w') as outfile:
+    json.dump(headlines, outfile)
